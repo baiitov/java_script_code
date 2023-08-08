115f310fdd70848b82a431b3145dd1b22cac4ec0
Bilal <b1335190@gmail.com>
2023-08-08 21:19:39 +0600

    'timer'

---
+++ timer/main.js
@@ -0,0 +1,5 @@
+const timeId = setInterval(() => {
+	const div = document.getElementById('div-main')
+	const color = Math.floor(Math.random() * 12345678).toString(16)
+	div.style.background = '#' + color
+}, 1000)
